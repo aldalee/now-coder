ef2ac54360b1eb4428e92e69f4ca8a37ea83868a
aldalee <hadoop_2020@163.com>
2023-02-07 12:01:26 +0800

    BM01反转链表

--- Interview-top101/T01_linkedlist/bm01_reverse_list.go
+++ Interview-top101/T01_linkedlist/bm01_reverse_list.go
@@ -6,13 +6,13 @@ type ListNode struct {
 }
 
 func ReverseList(pHead *ListNode) *ListNode {
-	var pre *ListNode = nil
+	var pre *ListNode = nil //新链表的头节点
 	var cur = pHead
 	for cur != nil {
-		curNext := cur.Next
-		cur.Next = pre
-		pre = cur
-		cur = curNext
+		next := cur.Next //保存当前节点的next指针
+		cur.Next = pre   //当前节点反转
+		pre = cur        //更新新链表的头节点
+		cur = next       //继续下一次调整
 	}
 	return pre
 }
