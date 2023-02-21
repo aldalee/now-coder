f3ffa0d30d1c0d68543b9e3d33ead9c908e4e45f
aldalee <hadoop_2020@163.com>
2023-02-21 13:07:54 +0800

    自定义min、max函数

---
+++ Interview-top101/tools/tool.go
@@ -0,0 +1,21 @@
+package tools
+
+func Max(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v > res {
+			res = v
+		}
+	}
+	return res
+}
+
+func Min(a ...int) int {
+	res := a[0]
+	for _, v := range a[1:] {
+		if v < res {
+			res = v
+		}
+	}
+	return res
+}
